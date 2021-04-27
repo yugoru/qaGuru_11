3e82e480888aeb98aeae74284725324adbc2b2b7
yugoru <yulia.gorshkova@soft-machine.ru>
2021-04-27 17:47:01 +0300

    Добавлены свойства драйвера

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -2,7 +2,7 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({ "classpath:config/driver.properties" })
+@Config.Sources({ "classpath:config/driver_example.properties" })
 public interface DriverConfig extends Config {
     @Key("remote.web.user")
     String remoteWebUser();
