eda247721199f0be3c5b2dcbf6507988b6238a25
yugoru <yulia.gorshkova@soft-machine.ru>
2021-04-27 16:49:30 +0300

    Добавление параметров

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         Configuration.browser = System.getProperty("web.browser", "chrome");
 
